020d3a0f356b6456c2b67988924f13d9beac297f
LuisJoseSC <lj.sanchezc1@uniandes.edu.co>
2025-11-27 00:44:14 -0500

    No programen en Java por favor.

--- 2025/Noviembre/26 de noviembre/division.java
+++ 2025/Noviembre/26 de noviembre/division.java
@@ -1,5 +1,5 @@
 public class division {
-    public static int resta ( int a, int b) {
+    public static int dividir ( int a, int b) {
         return a/b ;
     }
 }
--- 2025/Noviembre/26 de noviembre/main.java
+++ 2025/Noviembre/26 de noviembre/main.java
@@ -1,3 +1,44 @@
+import java.util.Scanner;
+
 public class main {
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Calculadora de mierda pq java es una mierda.");
+        System.out.println("1. Suma");
+        System.out.println("2. Resta");
+        System.out.println("3. Multiplicación");
+        System.out.println("4. División");
+        System.out.print("Seleccione la operación: ");
+        int op = sc.nextInt();
+
+        System.out.print("Ingrese el primer número: ");
+        int a = sc.nextInt();
+
+        System.out.print("Ingrese el segundo número: ");
+        int b = sc.nextInt();
+
+        int resultado = 0;
+
+        switch (op) {
+            case 1:
+                resultado = suma.sumar(a, b);
+                break;
+            case 2:
+                resultado = resta.restar(a, b);
+                break;
+            case 3:
+                resultado = multiplicacion.multiplicar(a, b);
+                break;
+            case 4:
+                resultado = division.dividir(a, b);
+                break;
+            default:
+                System.out.println("Opción inválida.");
+                return;
+        }
+
+        System.out.println("Resultado: " + resultado);
+    }
 }
