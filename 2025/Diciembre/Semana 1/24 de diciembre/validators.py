e31eb06639947191060c0f6e29e4c54e00e01662
LuisJoseSC <lj.sanchezc1@uniandes.edu.co>
2025-12-24 04:27:21 -0500

    Update Validators.py

--- 2025/Diciembre/Semana 1/24 de diciembre/validators.py
+++ 2025/Diciembre/Semana 1/24 de diciembre/validators.py
@@ -1,3 +1,16 @@
+import math
+from typing import Tuple
 
-def validate_coefficientes(a: float, b: float, c: float) -> None:
+def validate_coefficients(a,b,c) -> Tuple [float, float, float]:
+    try:
+        a = float(a)
+        b = float(b)
+        c = float(c)
+    except (TypeError, ValueError):
+        raise ValueError("Cofficients must be numeric values.")
     
+    if not all(math.isfinite(x) for x in (a,b,c)):
+        raise ValueError("Coffecients must be finite numbers.")
+    if a == 0:
+        raise ValueError("Coefficien 'a' must be non-zero for a quadratic equation. ")
+    return a,b,c
